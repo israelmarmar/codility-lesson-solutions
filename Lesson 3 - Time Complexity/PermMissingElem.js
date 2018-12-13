d742b61f0603ded6115be067832a8198a986a586
Israel Marques Martins <email.israel.br@gmail.com>
2018-12-13 09:16:23 -0400

    Update PermMissingElem.js

--- Lesson 3 - Time Complexity/PermMissingElem.js
+++ Lesson 3 - Time Complexity/PermMissingElem.js
@@ -1,1 +1,13 @@
 
+function solution(A){
+    var n = A.length;
+    var sum=0;
+
+    for(var i=0;i<n;i++)
+        sum=sum+A[i];
+
+    return (n+1)*(n+2)/2-sum;
+
+}
+
+//https://app.codility.com/demo/results/training223GTE-AGQ/
